eb5ecaf167b421e415e9b7ad3b2156e6c3aad222
futureprogrammer360 <86537525+futureprogrammer360@users.noreply.github.com>
2021-08-21 15:59:00 -0700

    Change how moving cursor backward after print statement insert is handled

--- print_debugger.py
+++ print_debugger.py
@@ -21,28 +21,35 @@ class PrintDebugCommand(sublime_plugin.TextCommand):
 
         # Determine debug and print statements
         debug_statement = current_syntax["debug"]
-        print_statement, back = current_syntax["print"]
+        print_statement = current_syntax["print"]
         if settings.get("force_double_quotes"):
             debug_statement = debug_statement.replace("'", '"')
             print_statement = print_statement.replace("'", '"')
 
         region = self.view.sel()[0]
         if region.begin() == region.end():  # No selection
-            selection = self.view.substr(self.view.word(region))
-            if not selection.strip():  # Cursor not on a word, insert empty print statement
+            selection = self.view.substr(self.view.word(region))  # Text under cursor
+
+            # Cursor not on a word, insert empty print statement
+            if not selection.strip():
+                back = len(print_statement) - print_statement.index("$0") - 2
+                print_statement = print_statement.replace("$0", "")
                 self.view.run_command("insert", {"characters": print_statement})
                 for _ in range(back):
                     self.view.run_command("move", {"by": "characters", "forward": False})
                 return
-        else:
-            selection = self.view.substr(region)
 
+        else:
+            selection = self.view.substr(region)  # Text selected
+
+        # Insert debug statement
+        debug_statement = debug_statement.replace("!!!", selection)
         if before_and_after:
             self.view.run_command("run_macro_file", {"file": "res://Packages/Default/Add Line Before.sublime-macro"})
-            self.view.run_command("insert", {"characters": debug_statement.replace("!!!", selection)})
+            self.view.run_command("insert", {"characters": debug_statement})
             self.view.run_command("move", {"by": "lines", "forward": True})
             self.view.run_command("run_macro_file", {"file": "res://Packages/Default/Add Line.sublime-macro"})
-            self.view.run_command("insert", {"characters": debug_statement.replace("!!!", selection)})
+            self.view.run_command("insert", {"characters": debug_statement})
         else:
             self.view.run_command("run_macro_file", {"file": "res://Packages/Default/Add Line.sublime-macro"})
-            self.view.run_command("insert", {"characters": debug_statement.replace("!!!", selection)})
+            self.view.run_command("insert", {"characters": debug_statement})
